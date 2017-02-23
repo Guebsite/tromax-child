836e36ba11e6e344dd63b91237a89831618016a2
@ReynierNR <reynier@Reynier.local>
2017-02-22 19:42:31 -0700

    Compro vendo y Rento
    Arreglado

---
+++ cuadros.php
@@ -0,0 +1,32 @@
+    <section id="opciones">
+       <h5>Que deseas Hacer</h5>
+       <h5 class="elegir">elige una opción</h5>
+        <div class="ajuste">
+            <div class="quiero rentar">
+                <a href="#">
+                    <h6 class="titulos">Quiero Rentar</h6>
+                    <p>R</p>
+                </a>
+            </div>           
+            
+        </div>
+        
+        <div class="ajuste">
+            <div class="quiero">
+                <a href="#">
+                    <h6 class="titulos">Quiero Vender</h6>
+                    <p>V</p>
+                </a>
+            </div>           
+        </div>
+        
+        <div class="ajuste">
+            <div class="quiero comprar">
+                <a href="#">
+                    <h6 class="titulos">Quiero Comprar</h6>
+                    <p>C</p>
+                </a>
+            </div>           
+        </div>
+        
+    </section>
