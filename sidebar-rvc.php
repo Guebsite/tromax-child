5f63164dbeb304f2f44779095707b8e658ef05e6
reynier-nr <reynier.nr@gmail.com>
2017-03-13 08:53:42 -0700

    Plantillas
    iniciando plantillas

---
+++ sidebar-rvc.php
@@ -0,0 +1,10 @@
+<div id="secondary" class="widget-area <?php apply_filters('tromax_secondary-width','tromax_secondary_class') ?>" role="complementary">
+	
+	<h1>Contáctnos</h1>
+	
+	
+	
+	
+	
+</div><!-- #secondary -->
+
