a20fa39a06dc208f4668d4de0c8dc311886d8796
reynier-nr <reynier.nr@gmail.com>
2017-03-29 00:13:14 -0700

    short code contact form

--- enviar.php
+++ enviar.php
@@ -1,12 +1,13 @@
 <?php 
 $destino = "reynier.nr@gmail.com";
-$subject = "www.elyfotografia.com.mx";
+$subject = "www.rentobregon.com.mx";
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $tel = $_POST["tel"];
+$quiero = $_POST["quiero"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Mensaje_desde" . $subject .  "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . tel . "\nMensaje: " . $mensaje;
+$contenido = "Mensaje_desde" . $subject .  "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . tel . "\nQuiero: " . quiero . "\nMensaje: " . $mensaje;
 mail($destino,"Contacto", $contenido);
-header("Location:enviado.html");
+header("Location:enviado.php");
 
 ?>
