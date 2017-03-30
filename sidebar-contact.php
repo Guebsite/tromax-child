33e8b313d7d4e07d5a9af6a9771ef44e196c91c5
@ReynierNR <reynier@Reynier.local>
2017-03-29 17:34:18 -0700

    forms comprar, vender y rentar

--- sidebar-contact.php
+++ sidebar-contact.php
@@ -1,8 +1,8 @@
 <div id="secondary" class="widget-area <?php apply_filters('tromax_secondary-width','tromax_secondary_class') ?>" role="complementary">
 	
-	<?php echo do_shortcode( '[contact-form-7 id="157" title="Contáctanos"]' ); ?>
-
 	
+	<?php echo do_shortcode( '[contact-form-7 id="161" title="Contáctanos"]' ); ?>
+
 	
 	
 	
