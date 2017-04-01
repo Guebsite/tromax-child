b4b366d94b60cda67d72118070e64cf35eece868
@ReynierNR <reynier@Reynier.local>
2017-03-31 18:04:29 -0700

    arreglos

--- cuadros.php
+++ cuadros.php
@@ -2,11 +2,9 @@
        <h5>¿Que deseas hacer?</h5>
         <div class="ajuste">
             <div class="quiero rentar">
-<<<<<<< Updated upstream
+
                 <a href="http://localhost/ProyectosGuebsite/rentobr/quiero-rentar/">
-=======
-                <a href="http://www.rentobregon.com.mx/quiero-rentar/">
->>>>>>> Stashed changes
+
                     <h6 class="titulos">Quiero Rentar</h6>
                     <p>R</p>
                 </a>
@@ -16,11 +14,9 @@
         
         <div class="ajuste">
             <div class="quiero">
-<<<<<<< Updated upstream
+
                 <a href="http://localhost/ProyectosGuebsite/rentobr/quiero-vender/">
-=======
-                <a href="http://www.rentobregon.com.mx/quiero-vender/">
->>>>>>> Stashed changes
+
                     <h6 class="titulos">Quiero Vender</h6>
                     <p>V</p>
                 </a>
@@ -29,11 +25,9 @@
         
         <div class="ajuste">
             <div class="quiero comprar">
-<<<<<<< Updated upstream
+
                 <a href="http://localhost/ProyectosGuebsite/rentobr/quiero-comprar/">
-=======
-                <a href="http://www.rentobregon.com.mx/quiero-comprar/">
->>>>>>> Stashed changes
+
                     <h6 class="titulos">Quiero Comprar</h6>
                     <p>C</p>
                 </a>
