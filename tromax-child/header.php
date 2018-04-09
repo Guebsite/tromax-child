59666af3c280bc72a87ec4ddbb86bdf54c2ae7f2
@ReynierNR <reynier.nr@gmail.com>
2018-04-08 22:12:33 -0700

    google adsense
    agregando google adsence

--- tromax-child/header.php
+++ tromax-child/header.php
@@ -15,6 +15,13 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7007902074030401",
+    enable_page_level_ads: true
+  });
+</script>
 </head>
 
 <body <?php body_class(); ?>>
